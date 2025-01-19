68ea10e884c94c606d3e1e637c73a1d485e22371
Yashowardhan <yashowardhanshinde@gmail.com>
2025-01-18 18:18:08 -0800

    Baseclass explanation

--- swiftannotate/image/captioning.py
+++ swiftannotate/image/captioning.py
@@ -3,22 +3,68 @@ import transformers
 import torch
 import logging
 from PIL import Image
-from typing import List
+from typing import List, Tuple
 import base64
 
 class BaseImageCaptioning:
+    """
+    Base class for image captioning. The class provides a blueprint for all image captioning models.
+    Each base class must implement the following methods:
+    - caption: generates a caption for an image
+    - validate: validates a caption for an image
+    - generate: generates captions for a list of images
+    """
     
-    def caption(self, image: Image) -> str:
+    def caption(self, image: Image, **kwargs) -> str:
+        """
+        Generates a caption for an image. Implements the logic to generate a caption for an image.
+
+        Args:
+            image (PIL.Image): Image to generate a caption for.
+            **kwargs: Additional arguments to pass to the method for API calls.
+
+        Raises:
+            NotImplementedError: Must be implemented in every subclass.
+
+        Returns:
+            str: Caption of the image.
+        """
         raise NotImplementedError("caption method must be implemented in every subclass")
     
-    def validate(self, image: Image, caption: str) -> bool:
+    def validate(self, image: Image, caption: str, **kwargs) -> Tuple[str, float]:
+        """
+        Validates a caption for an image. Implements the logic to validate a caption for an image.
+
+        Args:
+            image (Image): Image to validate the caption for.
+            caption (str): Generated caption for the image.
+            **kwargs: Additional arguments to pass to the method for API calls.
+
+        Raises:
+            NotImplementedError: Must be implemented in every subclass.
+
+        Returns:
+            Tuple[str, float]: Returns validation logic and confidence score ranging 0-1.
+        """
         raise NotImplementedError("validate method must be implemented in every subclass")
     
-    def generate(self, image_paths: List[str]) -> List[str, float]:
+    def generate(self, image_paths: List[str], **kwargs) -> List[Tuple[str, str, float]]:
+        """
+        Generates captions for a list of images. Implements the logic to generate captions for a list of images.
+
+        Args:
+            image_paths (List[str]): List of image paths to generate captions for.
+            **kwargs: Additional arguments to pass to the method for custom pipeline interactions.
+
+        Raises:
+            NotImplementedError: Must be implemented in every subclass.
+
+        Returns:
+            List[Tuple[str, str, float]]: List of captions, validation reasoning and confidence scores for each image.
+        """
         raise NotImplementedError("generate method must be implemented in every subclass")
     
-
-def ImageCaptionOpenAI(BaseImageCaptioning):
+class ImageCaptionOpenAI(BaseImageCaptioning):
     def __init__(
         self, 
         model: str, 
@@ -42,14 +88,13 @@ def ImageCaptionOpenAI(BaseImageCaptioning):
     def caption(self, image: Image, **kwargs) -> str:
         return 
     
-    def validate(self, image: Image, caption: str, threshold: float, **kwargs) -> bool:
+    def validate(self, image: Image, caption: str, threshold: float, **kwargs) -> Tuple[str, float]:
         return 
     
-    def generate(self, image_paths: List[str]) -> List[str, float]:
+    def generate(self, image_paths: List[str]) -> List[Tuple[str, str, float]]:
         return 
         
-
-def ImageCaptionHuggingFace(BaseImageCaptioning):
+class ImageCaptionHuggingFace(BaseImageCaptioning):
     def __init__(
         self, 
         model: str, 
@@ -64,8 +109,8 @@ def ImageCaptionHuggingFace(BaseImageCaptioning):
     def caption(self, image: Image, **kwargs) -> str:
         return 
     
-    def validate(self, image: Image, caption: str, threshold: float, **kwargs) -> bool:
+    def validate(self, image: Image, caption: str, threshold: float, **kwargs) -> Tuple[str, float]:
         return 
     
-    def generate(self, image_paths: List[str]) -> List[str, float]:
+    def generate(self, image_paths: List[str]) ->List[Tuple[str, str, float]]:
         return
